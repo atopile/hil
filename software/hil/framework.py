157c304cbc0986094598369818a1d9bba0686604
Matt Wildoer <matt@atopile.io>
2025-02-12 13:58:18 -0800

    Very WIP. Handoff to Sam

--- software/hil/framework.py
+++ software/hil/framework.py
@@ -1,7 +1,9 @@
 import asyncio
-from datetime import datetime
+from datetime import datetime, timedelta
 import inspect
-from typing import AsyncGenerator, Awaitable, Callable, Self
+from numbers import Number
+from typing import Any, AsyncGenerator, Awaitable, Callable, Self
+import numpy as np
 import polars as pl
 
 
@@ -39,14 +41,109 @@ class during:
     def remaining(self) -> float:
         return self.duration - self.elapsed
 
-    async def any(self, *others: Callable[[], Awaitable]) -> AsyncGenerator[None, None]:
+    async def any(self, *others: Callable[[], Awaitable] | AsyncGenerator[Any, None]) -> AsyncGenerator[None, None]:
+        def _make_awaitable(other: Callable[[], Awaitable] | AsyncGenerator[Any, None]) -> Awaitable[Any]:
+            if inspect.iscoroutinefunction(other):
+                return other()
+            elif inspect.isasyncgenfunction(other):
+                return other
+            else:
+                raise ValueError(f"Invalid argument: {other}")
+
         async for _ in self:
             await asyncio.wait(
-                [other() for other in others],
+                [_make_awaitable(other) for other in others],
                 timeout=self.remaining,
                 return_when=asyncio.FIRST_COMPLETED,
             )
             yield
+
+
+class Trace[T]:
+    TIMESTAMP_COLUMN = "timestamp"
+
+    def __init__(self, name: str, data: pl.DataFrame | None = None):
+        self._name = name
+        self._data: list[T] = []
+        self._timestamps: list[datetime] = []
+        self._polars: pl.DataFrame | None = data
+        self._result_future: asyncio.Future[T] | None = None
+
+    def append(self, timestamp: datetime, data: T) -> None:
+        self._timestamps.append(timestamp)
+        self._data.append(data)
+
+        if self._result_future is not None:
+            self._result_future.set_result(data)
+            self._result_future = None
+
+    def to_polars(self) -> pl.DataFrame:
+        new_df = pl.DataFrame(
+            {self.TIMESTAMP_COLUMN: self._timestamps, self._name: self._data}
+        )
+        self._timestamps.clear()
+        self._data.clear()
+
+        if self._polars is not None:
+            self._polars.extend(new_df)
+        else:
+            self._polars = new_df
+
+        return self._polars
+
+    def derive(self, data: pl.DataFrame) -> Self:
+        return self.__class__(self._name, data)
+
+    def get_last(
+        self, duration: float | int | timedelta, from_: datetime | None = None
+    ) -> Self:
+        """Get the last <duration> of data from the trace"""
+        if isinstance(duration, Number):
+            duration = timedelta(seconds=duration)
+
+        if from_ is None:
+            from_ = datetime.now() - duration
+
+        return self.derive(
+            self.to_polars()
+            .sort(self.TIMESTAMP_COLUMN)
+            .filter(pl.col(self.TIMESTAMP_COLUMN) >= from_)
+        )
+
+    @property
+    def timestamps(self) -> pl.Series:
+        return self.to_polars().get_column(self.TIMESTAMP_COLUMN)
+
+    @property
+    def data(self) -> pl.Series:
+        return self.to_polars().get_column(self._name)
+
+    @property
+    def duration(self) -> timedelta:
+        return self.timestamps.max() - self.timestamps.min()
+
+    @property
+    def duration_s(self) -> float:
+        return self.duration.total_seconds()
+
+    def new_data(self) -> asyncio.Future[T]:
+        if self._result_future is None:
+            self._result_future = asyncio.Future()
+        return self._result_future
+
+    async def __anext__(self) -> T:
+        return await self.new_data()
+
+    def __gt__(self, other: Any) -> bool:
+        if isinstance(other, datetime):
+            return self.get_last()
+        return np.all(self.duration > other)
+
+    def __ge__(self, other: Any) -> bool:
+        return self.duration >= other
+
+    def __lt__(self, other: Any) -> bool:
+        return self.duration < other
 
 
 class record[T]:
@@ -66,16 +163,13 @@ class record[T]:
         else:
             self._source = lambda: asyncio.to_thread(source)
 
-        self._data: list[T] = []
-        self._timestamps: list[datetime] = []
-        self._trace: pl.DataFrame | None = None
-        self._name = name or source.__name__
-        self._result_future: asyncio.Future[T] | None = None
+        self._trace = Trace[T](name or source.__name__)
         self._minimum_interval = minimum_interval
+        self._last_timestamp: datetime | None = None
 
-    def __enter__(self) -> Self:
+    def __enter__(self) -> Trace[T]:
         self.start()
-        return self
+        return self.trace
 
     def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.close()
@@ -83,20 +177,18 @@ class record[T]:
     def start(self) -> None:
         async def _trace() -> None:
             while True:
-                if self._minimum_interval is not None and self._timestamps:
-                    last_timestamp = self._timestamps[-1]
+                if (
+                    self._minimum_interval is not None
+                    and self._last_timestamp is not None
+                ):
                     remaining_time_to_next = (
-                        last_timestamp + self._minimum_interval - datetime.now()
+                        self._last_timestamp + self._minimum_interval - datetime.now()
                     )
                     if remaining_time_to_next > 0:
                         await asyncio.sleep(remaining_time_to_next)
 
                 data = await self._source()
-                self._data.append(data)
-                self._timestamps.append(datetime.now())
-                if self._result_future is not None:
-                    self._result_future.set_result(data)
-                    self._result_future = None
+                self._trace.append(datetime.now(), data)
 
         self._task = asyncio.create_task(_trace())
 
@@ -104,14 +196,7 @@ class record[T]:
         self._task.cancel()
 
     @property
-    def trace(self) -> pl.DataFrame:
-        new_df = pl.DataFrame({"timestamp": self._timestamps, self._name: self._data})
-        self._timestamps.clear()
-        self._data.clear()
-        if self._trace is not None:
-            self._trace.extend(new_df)
-        else:
-            self._trace = new_df
+    def trace(self) -> Trace[T]:
         return self._trace
 
     @property
@@ -126,13 +211,9 @@ class record[T]:
     def running(self) -> bool:
         return self.started and not self.finished
 
-    def new_data(self) -> asyncio.Future[T]:
-        if not self.running:
-            raise RuntimeError("Recording not running")
-
-        if self._result_future is None:
-            self._result_future = asyncio.Future()
-        return self._result_future
+    @classmethod
+    def from_property(cls, obj: Any, prop: property) -> Self:
+        return cls(lambda: prop.fget(obj))
 
     async def __anext__(self) -> T:
         if not self.started:
@@ -141,4 +222,4 @@ class record[T]:
         if self.finished:
             raise StopAsyncIteration
 
-        return await self.new_data
+        return await self.trace.new_data()
--- tests/test_demo.py
+++ tests/test_demo.py
@@ -16,8 +16,12 @@ async def source_2() -> float:
 
 async def test_demo():
     with record(source_1) as t1, record(source_2) as t2:
-        async for _ in during(3.0).any(t1.new_data, t2.new_data):
+        async for _ in during(1).any(t1.new_data, t2.new_data):
             print(f"something's ready! {datetime.now()}")
+            if t1.duration_s > 0.5 and t1.get_last(0.5) > 1.0 and t2.duration_s > 0.5 and t2.get_last(0.5) > 1.0:
+                break
+        else:
+            raise TimeoutError("Failed to settle in time")
 
-    print(t1.trace)
-    print(t2.trace)
+    print(t1)
+    print(t2)
