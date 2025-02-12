f94a14a6b8bbf5690dbd7e45bc887974393e4d8d
Matt Wildoer <matt@atopile.io>
2025-02-12 10:24:18 -0800

    Add HiL framework

---
+++ software/hil/framework.py
@@ -0,0 +1,121 @@
+import asyncio
+from datetime import datetime
+from typing import AsyncGenerator, Awaitable, Callable, Self
+import polars as pl
+
+
+class during:
+    def __init__(self, duration: float):
+        self.duration = duration
+        self.start_time = None
+
+    def start(self) -> Self:
+        self.start_time = datetime.now()
+        return self
+
+    async def __aiter__(self):
+        if not self.started:
+            self.start()
+
+        while not self.finished:
+            yield
+            # yield to other tasks
+            await asyncio.sleep(0)
+
+    @property
+    def started(self) -> bool:
+        return self.start_time is not None
+
+    @property
+    def finished(self) -> bool:
+        return self.started and self.remaining <= 0
+
+    @property
+    def elapsed(self) -> float:
+        return (datetime.now() - self.start_time).total_seconds()
+
+    @property
+    def remaining(self) -> float:
+        return self.duration - self.elapsed
+
+    async def any(self, *others: Callable[[], Awaitable]) -> AsyncGenerator[None, None]:
+        async for _ in self:
+            await asyncio.wait(
+                [other() for other in others],
+                timeout=self.remaining,
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            yield
+
+
+class record[T]:
+    def __init__(self, source: Callable[[], Awaitable[T]], name: str | None = None):
+        self._task: asyncio.Task | None = None
+        self._source = source
+        self._data: list[T] = []
+        self._timestamps: list[datetime] = []
+        self._trace: pl.DataFrame | None = None
+        self._name = name or source.__name__
+        self._result_future: asyncio.Future[T] | None = None
+
+    def __enter__(self) -> Self:
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
+        self.close()
+
+    def start(self) -> None:
+        async def _trace() -> None:
+            while True:
+                data = await self._source()
+                self._data.append(data)
+                self._timestamps.append(datetime.now())
+                if self._result_future is not None:
+                    self._result_future.set_result(data)
+                    self._result_future = None
+
+        self._task = asyncio.create_task(_trace())
+
+    def close(self) -> None:
+        self._task.cancel()
+
+    @property
+    def trace(self) -> pl.DataFrame:
+        new_df = pl.DataFrame({"timestamp": self._timestamps, self._name: self._data})
+        self._timestamps.clear()
+        self._data.clear()
+        if self._trace is not None:
+            self._trace.extend(new_df)
+        else:
+            self._trace = new_df
+        return self._trace
+
+    @property
+    def started(self) -> bool:
+        return self._task is not None
+
+    @property
+    def finished(self) -> bool:
+        return self.started and self._task.done()
+
+    @property
+    def running(self) -> bool:
+        return self.started and not self.finished
+
+    def new_data(self) -> asyncio.Future[T]:
+        if not self.running:
+            raise RuntimeError("Recording not running")
+
+        if self._result_future is None:
+            self._result_future = asyncio.Future()
+        return self._result_future
+
+    async def __anext__(self) -> T:
+        if not self.started:
+            raise RuntimeError("Recording not started")
+
+        if self.finished:
+            raise StopAsyncIteration
+
+        return await self.new_data
