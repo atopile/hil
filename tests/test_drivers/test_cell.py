bdc64af6d3e377652d97b1ba777f0ed17ac4c486
Matt Wildoer <matt@atopile.io>
2025-02-12 18:32:03 -0800

    Upgrade tests

--- tests/test_drivers/test_cell.py
+++ tests/test_drivers/test_cell.py
@@ -1,26 +1,24 @@
-from hil.drivers.aiosmbus2 import AsyncSMBus
+from hil.drivers.aiosmbus2 import AsyncSMBusPeripheral
 from hil.drivers.cell import Cell
 
 
 async def test_performance():
-    bus = await AsyncSMBus.create(1)
-    cells: list[Cell] = [Cell(x, bus) for x in range(0, 8)]
+    async with AsyncSMBusPeripheral(1) as bus:
+        cells: list[Cell] = [Cell(x, bus) for x in range(0, 8)]
 
-    for cell in cells:
-        await cell.init()
-
-    for _ in range(10):
         for cell in cells:
-            await cell.enable()
-            await cell.set_voltage(1)
-            await cell.turn_on_output_relay()
-            await cell.turn_on_load_switch()
+            await cell.setup()
 
-            await cell.get_voltage()
-            await cell.get_current()
+        for _ in range(10):
+            for cell in cells:
+                await cell.enable()
+                await cell.set_voltage(1)
+                await cell.turn_on_output_relay()
+                await cell.turn_on_load_switch()
 
-            await cell.turn_off_load_switch()
-            await cell.turn_off_output_relay()
-            await cell.disable()
+                await cell.get_voltage()
+                await cell.get_current()
 
-    await bus.close()
+                await cell.turn_off_load_switch()
+                await cell.turn_off_output_relay()
+                await cell.disable()
