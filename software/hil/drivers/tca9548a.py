cf13d163ddb287f3f24138716121dda243d9b75f
Matt Wildoer <matt@atopile.io>
2025-02-12 17:45:02 -0800

    Add TCA9548A I2C multiplexer driver
    Implement a basic driver for the TCA9548A I2C multiplexer with async support, allowing channel selection via the AsyncSMBus interface.

---
+++ software/hil/drivers/tca9548a.py
@@ -0,0 +1,21 @@
+import logging
+from hil.drivers.aiosmbus2 import AsyncSMBus
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_MUX_ADDRESS = 0x70
+
+
+class TCA9548A:
+    def __init__(self, bus: AsyncSMBus, address: int = DEFAULT_MUX_ADDRESS):
+        self.bus = bus
+        self.address = address
+
+    async def set_mux(self, channel: int):
+        """
+        Select the correct MUX channel.
+        Writes 1 << mux_channel to the mux address.
+        """
+        value = 1 << channel
+        async with self.bus() as handle:
+            await handle.write_byte(self.address, value)
