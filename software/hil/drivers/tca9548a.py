fbbcea8c18f8d4c419f738043e0de031cde63376
Matt Wildoer <matt@atopile.io>
2025-02-12 18:39:18 -0800

    Skip mux update if we're already on the right channel

--- software/hil/drivers/tca9548a.py
+++ software/hil/drivers/tca9548a.py
@@ -10,12 +10,18 @@ class TCA9548A:
     def __init__(self, bus: AsyncSMBus, address: int = DEFAULT_MUX_ADDRESS):
         self.bus = bus
         self.address = address
+        self._current_channel: int | None = None
 
     async def set_mux(self, channel: int):
         """
         Select the correct MUX channel.
         Writes 1 << mux_channel to the mux address.
         """
+        if self._current_channel == channel:
+            return
+
         value = 1 << channel
         async with self.bus() as handle:
             await handle.write_byte(self.address, value)
+
+        self._current_channel = channel
