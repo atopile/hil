e755c3647fa60b4ee9018cbde0d5241b430c3c0d
Matt Wildoer <matt@atopile.io>
2025-02-12 19:21:44 -0800

    Try benchmarking instead

--- tests/test_drivers/test_cell.py
+++ tests/test_drivers/test_cell.py
@@ -1,19 +1,23 @@
+import asyncio
+
 from hil.drivers.aiosmbus2 import AsyncSMBusPeripheral, AsyncSMBusBranch
 from hil.drivers.cell import Cell
 from hil.drivers.tca9548a import TCA9548A
 
 
-async def test_performance():
+async def test_performance(benchmark):
     physical_bus = AsyncSMBusPeripheral(1)
     mux = TCA9548A(physical_bus)
     branch_buses = AsyncSMBusBranch.from_channels(physical_bus, mux, list(range(0, 8)))
-    cells: list[Cell] = [await Cell.create(i, bus) for i, bus in enumerate(branch_buses)]
+    cells: list[Cell] = [
+        await Cell.create(i, bus) for i, bus in enumerate(branch_buses)
+    ]
 
     async with physical_bus:
         for cell in cells:
             await cell.setup()
 
-        for _ in range(10):
+        async def _scan_cells():
             for cell in cells:
                 await cell.enable()
                 await cell.set_voltage(1)
@@ -26,3 +30,5 @@ async def test_performance():
                 await cell.turn_off_load_switch()
                 await cell.turn_off_output_relay()
                 await cell.disable()
+
+        benchmark(asyncio.get_event_loop().run_until_complete, _scan_cells())
