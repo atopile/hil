b9716a12d8ae0557fecba605b25f07abd99c7b47
tomwildoer <tom.wildoer@gmail.com>
2025-02-13 10:08:04 -0800

    add fixture for cellsim

--- tests/test_drivers/test_cell.py
+++ tests/test_drivers/test_cell.py
@@ -1,34 +1,72 @@
 import asyncio
-from hil.drivers.aiosmbus2 import AsyncSMBusBranch, AsyncSMBusPeripheral
+
+import pytest_asyncio
+from hil.drivers.aiosmbus2 import AsyncSMBus, AsyncSMBusBranch, AsyncSMBusPeripheral
 from hil.drivers.cell import Cell
 from hil.drivers.tca9548a import TCA9548A
 
 
-async def test_performance():
-    physical_bus = AsyncSMBusPeripheral(1)
-    mux = TCA9548A(physical_bus)
-    branch_buses = AsyncSMBusBranch.from_channels(physical_bus, mux, list(range(0, 8)))
-    cells: list[Cell] = [
-        await Cell.create(i, bus) for i, bus in enumerate(branch_buses)
-    ]
+class CellSim:
+    """
+    Simulates a cell for testing purposes.
+    """
 
-    async with physical_bus:
-        for cell in cells:
-            await cell.setup()
-            await cell.set_voltage(1)
+    bus: AsyncSMBus
+    cells: list[Cell]
+    _mux: TCA9548A
+    _branch_buses: AsyncSMBusBranch
 
-        for _ in range(10):
-            for cell in cells:
-                await cell.enable()
-                await cell.turn_on_output_relay()
-                await cell.close_load_switch()
+    @classmethod
+    async def create(cls, bus: AsyncSMBus):
+        self = cls()
+        self._mux = TCA9548A(bus)
+        self._branch_buses = AsyncSMBusBranch.from_channels(
+            bus, self._mux, list(range(0, 8))
+        )
+        self.cells = [
+            await Cell.create(i, bus) for i, bus in enumerate(self._branch_buses)
+        ]
+        return self
 
-            await asyncio.gather(
-                *[cell.get_voltage() for cell in cells],
-                *[cell.get_current() for cell in cells],
-            )
 
-            for cell in cells:
-                await cell.open_load_switch()
-                await cell.turn_off_output_relay()
-                await cell.disable()
+class Hil:
+    """
+    Simulates a HIL for testing purposes.
+    """
+
+    cellsim: CellSim
+
+    async def create(cls):
+        self = cls()
+        physical_bus = AsyncSMBusPeripheral(1)
+        self.cellsim = CellSim(physical_bus)
+        return self
+
+
+@pytest_asyncio.fixture(loop_scope="session")
+async def hil():
+    hil = await Hil.create()
+    async with hil.physical_bus:
+        yield hil
+
+
+async def test_performance(hil: Hil):
+    for cell in hil.cellsim.cells:
+        await cell.setup()
+        await cell.set_voltage(1)
+
+    for _ in range(10):
+        for cell in hil.cellsim.cells:
+            await cell.enable()
+            await cell.turn_on_output_relay()
+            await cell.close_load_switch()
+
+        await asyncio.gather(
+            *[cell.get_voltage() for cell in hil.cellsim.cells],
+            *[cell.get_current() for cell in hil.cellsim.cells],
+        )
+
+        for cell in hil.cellsim.cells:
+            await cell.open_load_switch()
+            await cell.turn_off_output_relay()
+            await cell.disable()
