66ef00b98c024000c876dde90d59049ffc7c0d19
Matt Wildoer <matt@atopile.io>
2025-02-13 09:22:35 -0800

    Don't gather in test_cell, only 10x reading

--- tests/test_drivers/test_cell.py
+++ tests/test_drivers/test_cell.py
@@ -1,14 +1,6 @@
-from pathlib import Path
-
-# import pyinstrument
-import asyncio
-
-from hil.drivers.aiosmbus2 import AsyncSMBusPeripheral, AsyncSMBusBranch
+from hil.drivers.aiosmbus2 import AsyncSMBusBranch, AsyncSMBusPeripheral
 from hil.drivers.cell import Cell
 from hil.drivers.tca9548a import TCA9548A
-
-
-ARTIFACTS_DIR = Path(__file__).parent.parent.parent / "artifacts"
 
 
 async def test_performance():
@@ -19,32 +11,20 @@ async def test_performance():
         await Cell.create(i, bus) for i, bus in enumerate(branch_buses)
     ]
 
-    # profiler = pyinstrument.Profiler(interval=0.01)
-
     async with physical_bus:
         for cell in cells:
             await cell.setup()
+            await cell.enable()
+            await cell.set_voltage(1)
+            await cell.turn_on_output_relay()
+            await cell.close_load_switch()
 
-        # with profiler:
         for _ in range(10):
             for cell in cells:
-                await asyncio.gather(
-                    cell.enable(),
-                    cell.set_voltage(1),
-                    cell.turn_on_output_relay(),
-                    cell.close_load_switch(),
-                )
+                await cell.get_voltage()
+                await cell.get_current()
 
-                voltage, current = await asyncio.gather(
-                    cell.get_voltage(),
-                    cell.get_current(),
-                )
-
-                await asyncio.gather(
-                    cell.open_load_switch(),
-                    cell.turn_off_output_relay(),
-                    cell.disable(),
-                )
-
-    # ARTIFACTS_DIR.mkdir(parents=True, exist_ok=True)
-    # profiler.write_html(ARTIFACTS_DIR / "cell_performance.html")
+        for cell in cells:
+            await cell.open_load_switch()
+            await cell.turn_off_output_relay()
+            await cell.disable()
