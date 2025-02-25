b8d5fa80a6f1130c93f2739cc45688714277f763
Sam Mellor <sam@atopile.io>
2025-02-25 14:34:11 -0800

    ray -> http (wip)

--- software/hil/dist_plugin.py
+++ software/hil/dist_plugin.py
@@ -1,14 +1,12 @@
 from dataclasses import dataclass
+from enum import StrEnum, auto
 from pathlib import Path
+import time
 
 import pytest
 
-import ray
-import ray.util.queue
-import socket
 
-
-from _pytest.config import _prepareconfig
+PLUGIN_NAME = "dist"
 
 
 @dataclass
@@ -24,6 +22,14 @@ class RunsOn:
 
 
 NodeId = str
+
+
+class TestStatus(StrEnum):
+    # FIXME
+    Passed = auto()
+    Failed = auto()
+    Skipped = auto()
+    Error = auto()
 
 
 class Events:
@@ -42,25 +48,22 @@ class Events:
         report: pytest.TestReport
 
 
-# TODO: actor pool
-@ray.remote(resources={"remote": 1})
-class Remote:
-    """
-    Runs on remote node.
+class EndOfSession(Exception):
+    pass
 
-    Sets up pytest test loop. Receives test nodes to run.
+
+class Worker:
+    """
+    Runs on worker node once test session is started.
+
+    - retrieves active test session from server
+    - polls for allocated tests
+    - executes tests and reports results
+    - uploads artifacts at end of session
     """
 
-    def __init__(
-        self,
-        test_queue: ray.util.queue.Queue,
-        message_queue: ray.util.queue.Queue,
-        args: list[str],
-    ):
-        self.test_queue = test_queue
-        self.message_queue = message_queue
-        self.hostname = socket.gethostname()
-        self.config = _prepareconfig(args, [self])
+    def __init__(self, config: pytest.Config):
+        self.config = config
 
         # TODO: review
         # self.config.option.loadgroup = self.config.getvalue("dist") == "loadgroup"
@@ -72,81 +75,118 @@ class Remote:
         self.config.option.maxprocesses = None
         self.config.option.basetemp = Path.cwd() / "dist_tmp"
 
-        self.config.hook.pytest_cmdline_main(config=self.config)
-
     def process_test(self, nodeid_now: str, nodeid_next: str | None):
         item_now = self._items_by_nodeid[nodeid_now]
         item_next = self._items_by_nodeid[nodeid_next] if nodeid_next else None
         self.config.hook.pytest_runtest_protocol(item=item_now, nextitem=item_next)
+
+    def signal_ready(self): ...
+
+    def signal_done(self): ...
+
+    def fetch_work(self) -> tuple[NodeId, NodeId | None]: ...
+
+    def report_result(self, nodeid: NodeId, report: pytest.TestReport): ...
+
+    def upload_artifacts(self): ...
 
     @pytest.hookimpl(tryfirst=True)
     def pytest_runtestloop(self, session: pytest.Session):
         self.session = session
         self._items_by_nodeid = {item.nodeid: item for item in session.items}
 
-        self.message_queue.put(Events.Start(hostname=self.hostname))
-
-        # should be long enough to populate the queue
-        test_now = self.test_queue.get(block=True, timeout=30)
+        self.signal_ready()
 
         while True:
             try:
-                test_next = self.test_queue.get_nowait()
-                # TODO: shutdown signal
-            except ray.util.queue.Empty:
-                test_next = None
-
-            self.process_test(test_now, test_next)
-
-            if test_next is None:
+                nodeid_now, nodeid_next = self.fetch_work()
+                self.process_test(nodeid_now, nodeid_next)
+            except EndOfSession:
                 break
 
-            test_now = test_next
+        self.signal_done()
 
-        self.message_queue.put(Events.Finish(hostname=self.hostname))
         return True
 
     @pytest.hookimpl
     def pytest_runtest_logreport(self, report: pytest.TestReport) -> None:
-        self.message_queue.put(
-            Events.Report(hostname=self.hostname, nodeid=report.nodeid, report=report)
-        )
+        self.report_result(report.nodeid, report)
+
+    @pytest.hookimpl
+    def pytest_sessionfinish(self, session: pytest.Session, exitstatus: int):
+        self.upload_artifacts()
 
 
-class DSession:
+class TestResults:
+    nodeids: set[NodeId]
+    reports: dict[NodeId, pytest.TestReport]
+
+    def __init__(self, nodeids: set[NodeId]):
+        self.nodeids = nodeids
+        self.reports = {}
+
+    @property
+    def all_done(self) -> bool:
+        return len(self.reports) == len(self.nodeids)
+
+    def add(self, nodeid: NodeId, report: pytest.TestReport):
+        if nodeid not in self.nodeids:
+            raise ValueError(f"Unknown nodeid: {nodeid}")
+
+        if self.reports[nodeid] is not None:
+            raise ValueError(f"Test result already set for {nodeid}")
+
+        self.reports[nodeid] = report
+
+
+class Client:
+    """
+    Runs on test client.
+
+    - gets test session from server
+    - uploads runtime env
+    - send collected tests to server
+    - receives test results from server
+    - downloads artifacts
+    """
+
     runs_on_key = pytest.StashKey[dict[str, list[RunsOn]]]()
+    results: TestResults
+    statuses: dict[NodeId, TestStatus]
 
     def __init__(self, config: pytest.Config):
-        if not ray.is_initialized():
-            # TODO: from config
-            ray.init(
-                log_to_driver=False,  # hide worker output
-                address="ray://192.168.1.199:10001",
-                runtime_env={
-                    # `export RAY_RUNTIME_ENV_HOOK=ray._private.runtime_env.uv_runtime_env_hook.hook`
-                    # TODO: relative to something? project root?
-                    "working_dir": Path.cwd(),
-                    "py_modules": ["software/hil"],  # TODO: auto via uv
-                    # TODO: exclusions from file?
-                    "excludes": [
-                        "**/*.step",
-                        "**/*.wrl",
-                        "**/*.kicad_pcb",
-                        "**/*.kicad_pro",
-                        "**/*.kicad_sch",
-                        "**/fp-lib-table",
-                    ],
-                },
-            )
+        self.config = config
 
-        # TODO: queue per resource type
-        self.test_queue = ray.util.queue.Queue()
-        self.message_queue = ray.util.queue.Queue()
+    def submit_env(self): ...
 
-        args = [str(x) for x in config.invocation_params.args or ()]
+    def submit_tests(self, session: pytest.Session): ...
 
-        # TODO: remote per worker for each queue
-        self.remote1 = Remote.remote(self.test_queue, self.message_queue, args)
+    def fetch_results(self) -> list[pytest.TestReport]:
+        # TOOD: fetch new statuses
+        new_statuses: dict[NodeId, TestStatus] = {}
+        new_reports: list[pytest.TestReport] = []
+
+        for nodeid in new_statuses.keys() - self.statuses.keys():
+            new_report = self.fetch_report(nodeid)
+            self.results.add(nodeid, new_report)
+            new_reports.append(new_report)
+
+        self.statuses = new_statuses
+
+        return new_reports
+
+    def fetch_report(self, nodeid: NodeId) -> pytest.TestReport: ...
+
+    def download_artifacts(self): ...
+
+    @pytest.hookimpl
+    def pytest_sessionstart(self, session: pytest.Session):
+        # TODO: get session
+        self.submit_env()
+
+    @pytest.hookimpl
+    def pytest_sessionfinish(self, session: pytest.Session, exitstatus: int):
+        self.download_artifacts()
 
     @pytest.hookimpl(tryfirst=True)
     def pytest_collection(self, session: pytest.Session):
@@ -164,45 +204,30 @@ class DSession:
     def pytest_runtestloop(self, session: pytest.Session):
         # TODO: shutdown handling
 
-        reports_by_nodeid: dict[NodeId, pytest.TestReport | None] = {
-            item.nodeid: None for item in session.items
-        }
+        self.submit_tests(session)
 
-        # TODO: manage queue size
-        self.test_queue.put_nowait_batch([item.nodeid for item in session.items])
-
-        started = False
-        finished = False
+        self.results = TestResults(set(item.nodeid for item in session.items))
 
         while True:
-            try:
-                message = self.message_queue.get_nowait()
-                match message:
-                    case Events.Report(_, nodeid, report):
-                        reports_by_nodeid[nodeid] = report
-                        session.config.hook.pytest_runtest_logreport(report=report)
-                    case Events.Start(_):
-                        started = True
-                    case Events.Finish(_):
-                        assert started
-                        finished = True
-                    case _:
-                        print(f"unknown message: {message}")
-            except ray.util.queue.Empty:
-                message = None
-                if finished:
-                    break
+            new_reports = self.fetch_results()
+            for report in new_reports:
+                session.config.hook.pytest_runtest_logreport(report=report)
 
-        # TODO: better error
-        missing_reports = [
-            nodeid for nodeid, report in reports_by_nodeid.items() if report is None
-        ]
-        assert not missing_reports, f"No test report for: {', '.join(missing_reports)}"
+            if self.results.all_done:
+                break
+
+            time.sleep(1)  # FIXME
 
         return True
 
 
 @pytest.hookimpl(trylast=True)
 def pytest_configure(config):
-    session = DSession(config)
-    config.pluginmanager.register(session, "dist")
+    is_worker = config.getoption("worker")
+    session = Worker(config) if is_worker else Client(config)
+    config.pluginmanager.register(session, PLUGIN_NAME)
+
+
+@pytest.hookimpl
+def pytest_addoption(parser, pluginmanager):
+    parser.addoption("--worker", help="Run as a worker node", default=False)
