e05a28ebc6058b387fc88cfd45209789a0f07866
Matt Wildoer <matt@atopile.io>
2025-03-19 19:25:17 -0700

    Add runs_on tag

--- tests/test_drivers/test_mcp4728.py
+++ tests/test_drivers/test_mcp4728.py
@@ -1,11 +1,13 @@
 from typing import TYPE_CHECKING
 from hil.drivers.mcp4728 import MCP4728
+import pytest
 
 
 if TYPE_CHECKING:
     from ..conftest import SpaceBMSHiLDevV1
 
 
+@pytest.mark.runs_on(hostname="lively-sloth")
 async def test_mcp4728(spacebms_hil_dev_v1: SpaceBMSHiLDevV1):
     dac: MCP4728 = spacebms_hil_dev_v1.analog_out_4ch
     await dac.channel_a.set_voltage(1.23)
